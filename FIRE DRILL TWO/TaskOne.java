642a85e63a126bb767e2c5f6938e7ccb263dc540
Midemee <ashiwajuaramide@gmail.com>
2026-04-15 17:08:29 +0100

    Update

---
+++ FIRE DRILL TWO/TaskOne.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class TaskOne{
+    public static void main(String[] args){
+    Scanner userInput = new Scanner(System.in);
+
+    int count = 1;
+    int sum = 0;
+	while (count <= 10) {
+
+	System.out.print("Enter a score: ");
+    int score = userInput.nextInt();
+
+	sum = sum + score;
+    int total = sum;
+    count++;
+    System.out.print(total);
+	}
+	
+}
+}
+
---
+++ FIRE DRILL TWO/TaskThree.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class TaskThree{
+    public static void main(String[] args){
+    Scanner userInput = new Scanner(System.in);
+
+    int count = 1;
+    int sum = 0;
+	while (count <= 10) {
+
+	System.out.print("Enter a score: ");
+    int score = userInput.nextInt();
+
+	sum = sum + score;
+    int total = sum;
+    int average = total/count;
+    count++;
+    System.out.print(total);
+    System.out.print(average);
+	}
+	
+}
+}
